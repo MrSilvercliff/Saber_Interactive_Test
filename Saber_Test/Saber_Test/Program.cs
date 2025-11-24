0d0c17b22d73c8b1f915983f495c67b404177ce2
Mikhail <>
2025-11-24 13:08:51 +0300

    - выделил список для десериализации в отдельную переменную и немного сократил заполнение данных

--- Saber_Test/Saber_Test/Program.cs
+++ Saber_Test/Saber_Test/Program.cs
@@ -4,20 +4,25 @@ namespace Saber_Test
 {
     internal class Program
     {
-        private static ListRand _listRand;
+        private static ListRand _listRandSerialize;
+        private static ListRand _listRandDeserialize;
 
         private static void Main(string[] args)
         {
-            _listRand = new ListRand();
-            FillList();
+            _listRandSerialize = new ListRand();
+            _listRandDeserialize = new ListRand();
 
-            _listRand.Print();
+            FillList(_listRandSerialize);
+            _listRandSerialize.SetRands();
+
+            _listRandSerialize.Print();
+            Console.WriteLine();
 
             TestSerialize();
             TestDeserialize();
         }
 
-        private static void FillList()
+        private static void FillList(ListRand listRand)
         {
             var count = Random.Shared.Next(10, 16);
             var builder = new StringBuilder();
@@ -28,7 +33,7 @@ namespace Saber_Test
 
                 var node = new ListNode();
                 node.SetData(data);
-                _listRand.AddAfterTail(node);
+                listRand.AddAfterTail(node);
             }
         }
 
@@ -36,12 +41,7 @@ namespace Saber_Test
         {
             stringBuilder.Clear();
 
-            var now = DateTime.Now;
-            stringBuilder.Append($"{now}.{now.Millisecond} ");
-
-            var count = Random.Shared.Next(10, 21);
-
-            for (int i = 0; i < count; i++)
+            for (int i = 0; i < 4; i++)
             {
                 var randInt = Random.Shared.Next(1, 10);
                 stringBuilder.Append(randInt);
@@ -53,10 +53,40 @@ namespace Saber_Test
 
         private static void TestSerialize()
         {
+            FileStream? fileStream = null;
+
+            try
+            {
+                fileStream = new FileStream("ListRandData.dat", FileMode.OpenOrCreate, FileAccess.Write);
+                Console.WriteLine(fileStream.Name);
+                _listRandSerialize.Serialize(fileStream);
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                fileStream?.Close();
+            }
         }
 
         private static void TestDeserialize()
         {
+            FileStream? fileStream = null;
+
+            try
+            {
+                fileStream = new FileStream("ListRandData.dat", FileMode.OpenOrCreate, FileAccess.Write);
+                Console.WriteLine(fileStream.Name);
+                _listRandDeserialize.Deserialize(fileStream);
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                fileStream?.Close();
+            }
         }
     }
 }
